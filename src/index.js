c679b02949dac0852f86751844894b84769fb6b5
Bryan <[200~130257764+Awnnz@users.noreply.github.com>
2023-07-01 01:39:36 -0700

    add background to menu

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import initialLoad from './functions/intitial-load';
+import header from './pages/header';
 import clear from './functions/clear-page';
 import './styles/home.css';
 import './styles/menu.css';
